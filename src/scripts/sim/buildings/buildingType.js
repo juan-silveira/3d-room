3fe176357f1425a79b141e024e3c32aed0707c85
Juan Silveira <juan.silveira@starmountain.pt>
2025-04-28 15:45:43 -0300

    fixed test

--- src/scripts/sim/buildings/buildingType.js
+++
@@ -1,9 +0,0 @@
-export const BuildingType = {
-  residential: 'residential',
-  commercial: 'commercial',
-  industrial: 'industrial',
-  road: 'road',
-  powerPlant: 'power-plant',
-  powerLine: 'power-line',
-  cannabisPlant: 'cannabis-plant'
-}
